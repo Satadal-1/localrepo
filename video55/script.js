21b92a4c8eeaef4cb80bf76946b1937d5295744f
Satadal-1 <152809689+Satadal-1@users.noreply.github.com>
2024-10-04 10:40:48 +0530

    new files added

---
+++ video55/script.js
@@ -0,0 +1,33 @@
+console.log("Hey I am video 55")
+
+let a = 5;
+let b = 6;
+let c = "Satadal";
+var _c = "Arnab";
+// var 55a = 5;  Not valid
+console.log( a )
+console.log(typeof a, typeof b, typeof c)
+
+{
+    let a = 66;
+    console.log(a)
+}
+// Read the differences of let and var from codewith harry's notes
+console.log(a)
+const a1 = 11;
+// a1 = a1 + 2;  Not valid because a1 is constant
+ let x = 33;
+ let y = "Satadal Halder";
+ let z = undefined;
+ const p = true;
+ let m = null;
+ 
+ console.log(x,y,z,p,m);
+ console.log(typeof x,typeof y,typeof z,typeof p,typeof m)
+  let o ={
+    "name ": "Satadal",
+    "Company name": "Apple"
+  }
+  console.log(o)
+  o.streetname = "Unnyan Samiti"
+  console.log(o)
---
+++ video56/index.js
@@ -0,0 +1,10 @@
+console.log("Hii I am conditional Tutorial")
+
+let age = 45;
+ 
+if( age > 18){
+    console.log("You can drive");
+}
+else {
+    console.log("You cannot drive");
+}
