6aa97b542fa68597e3e73e6a41f5258ab1e5aac2
Satadal-1 <152809689+Satadal-1@users.noreply.github.com>
2024-10-19 18:16:00 +0530

    new files added

---
+++ video60/index.js
@@ -0,0 +1,22 @@
+console.log("This is Strings Tutorial")
+let a = "Satadal";
+console.log(a[0])
+console.log(a[1])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[7])
+let real_name= "Rahul"
+let friend= "Rohan"
+console.log("His name is " + real_name + " and his friend's name is "+ friend);
+console.log(`his name is ${real_name} and his friend's name is ${friend}`)
+let b = "Arnab";
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,4))
+console.log(b.slice(1))
+console.log(b.replace("Ar", "45"))
+console.log(b.concat(a, "Aishwaria","Rahul"))
+console.log(b)
