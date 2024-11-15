6ff280b3422d2eb1c390298887de8389702d988b
Satadal Halder <152809689+Satadal-1@users.noreply.github.com>
2024-11-15 11:08:36 +0530

    new file added

---
+++ video73/script.js
@@ -0,0 +1,10 @@
+let button = document.querySelector(".btn")
+
+button.addEventListener("click", ()=>{
+    document.querySelector(".box").innerHTML = "<b>Yeyyy you are clicked </b>Enjoy your click!!"
+    
+})
+button.addEventListener("contextMenu", ()=>{
+    alert = (" you are clicked")
+    
+})
