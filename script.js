abf535f2b239722b381726664bf054f2adac41f6
Satadal Halder <152809689+Satadal-1@users.noreply.github.com>
2024-12-07 14:28:36 +0530

    video 82 complete

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert("Welcome to Sigma Web Development Course")
+async function sleep() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(45)
+        }, 1000);
+    })
+}
+(async function main(){
+    // let a = await sleep();
+    // console.log(a)
+    // let [x , y,...rest]  = [5,4,7,8,32 ,0, 78,4]
+    // console.log(x,y,rest);
+    let obj = {
+        a : 1,
+        b : 2,
+        c : 3
+    }
+    let {a,b}= obj;
+    console.log(a,b);
+
+    let arr = [1, 2, 3];
+    console.log(sum(arr[0],arr[1],arr[2]))
+    console.log(sum(...arr))
+    
+})()
